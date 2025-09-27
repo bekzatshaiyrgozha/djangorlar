edba71bbe23378f33cadb6b3cced01f571633605
Your Name <your.email@example.com>
2025-09-27 13:36:10 +0500

    Finished changes in tester branch

--- settings/urls.py
+++ settings/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from apps.cards.views import index
+from apps.cards.views import users_list,city_time,counter_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index,name = 'index')
+    path('',index,name = 'index'),
+    path('users/', users_list,name = 'users_list'),
+    path('city_time/', city_time,name = 'city_time'),
+    path('cnt/', counter_view, name='counter'),
 ]
